8bac5eba063b5cc163e173a6b07d948be6b8b458
Aaron Loh <133678412+aaronloh16@users.noreply.github.com>
2024-08-11 23:51:34 -0400

    testing deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,15 @@ export function Home() {
 			<div style={{ paddingTop: '40px' }}>
 				<h1>Welcome to Paradise Nursery</h1>
 				<h2>Where Green Meets Serenity</h2>
-                <p> We offer a wide variety of plants that will help you create a serene environment in your home. <br /><br />Pleae take a look at all we have to offer and contact us if you need any assistance!</p>
-                   
+				<p>
+					{' '}
+					We offer a wide variety of beautiful plants that will help you create
+					a serene environment in your home. <br />
+					<br />
+					Pleae take a look at all we have to offer and contact us if you need
+					any assistance!
+				</p>
+
 				<img src={plant} />
 				<div style={{ display: 'flex', justifyContent: 'center' }}>
 					<Link to="/Product">
